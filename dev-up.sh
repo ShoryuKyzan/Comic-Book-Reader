bed00b5ad1a68206f0289bd1eb7fb171a2b67a2c
Joseph Fleming <32823296+ShoryuKyzan@users.noreply.github.com>
2020-03-11 18:45:34 -0400

    added health check loop to dev-up

--- dev-up.sh
+++ dev-up.sh
@@ -3,8 +3,14 @@
 docker-compose -f docker-compose-dev.yaml up -d
 pip install -r requirements.txt
 cd comicsite
+
 echo 'waiting for db to come up'
-sleep 15
+result=1
+while [[ "$result" -eq "1" ]]
+do
+    nc -z -v -w5 db 3306
+    result=$?
+done
 
 python manage.py makemigrations
 python manage.py migrate
